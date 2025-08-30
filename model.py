11dba13b453fe3350e8428acfca4611b197ff0a2
Radoslav Mráz <radoslav.mraz95@gmail.com>
2025-08-30 22:06:01 +0200

    Add domain classes and unit tests

---
+++ model.py
@@ -0,0 +1,86 @@
+from datetime import date, timedelta
+from dataclasses import dataclass, field
+from typing import NewType
+import queue
+
+OrderReference = NewType('OrderReference', str)
+
+@dataclass(frozen=True)
+class OrderLine:
+    order_reference: OrderReference
+    sku: str
+    qty: int
+
+
+@dataclass
+class Batch:
+    def __init__(self, reference: str, sku: str, qty: int, eta: date|None = None):
+        self.reference = reference
+        self.sku = sku
+        self.qty = qty
+        self.order_lines: set[OrderLine] = set()
+        self.eta = eta
+
+    def allocate(self, order_line: OrderLine) -> bool:
+        if order_line.sku != self.sku:
+            return False
+        if order_line.qty > self.available_qty:
+            return False
+        if order_line in self.order_lines:
+            return False
+        self.order_lines.add(order_line)
+        return True
+    
+    def deallocate(self, order_line: OrderLine) -> bool:
+        if not self.can_deallocate(order_line):
+            return False
+        self.order_lines.remove(order_line)
+        return True
+    
+    def can_allocate(self, order_line: OrderLine) -> bool:
+        return order_line.sku == self.sku and order_line not in self.order_lines and order_line.qty <= self.available_qty
+
+    def can_deallocate(self, order_line: OrderLine) -> bool:
+        return order_line in self.order_lines
+    
+    @property
+    def allocated_qty(self) -> int:
+        return sum(line.qty for line in self.order_lines)
+
+    @property
+    def available_qty(self) -> int:
+        return self.qty - self.allocated_qty
+    
+    def __hash__(self):
+        return hash(self.reference)
+    
+    def __eq__(self, value) -> bool:
+        if not isinstance(value, Batch):
+            return False
+        return value.reference == self.reference
+
+
+class AllocationService:
+    def __init__(self):
+        self.available_stocks: dict[str, queue.PriorityQueue[Batch]] = dict()
+        self.exhausted_stocks: dict[str, set[Batch]] = dict()
+    
+    def notify(self, batch: Batch):
+        if batch.eta is not None:
+            self.available_stocks.setdefault(batch.sku, queue.PriorityQueue()).put((1, batch.eta, batch))
+        else:
+            self.available_stocks.setdefault(batch.sku, queue.PriorityQueue()).put((0, None, batch))
+    
+    def put_order(self, order_line: OrderLine) -> bool:
+        if order_line.sku not in self.available_stocks:
+            return False
+        if self.available_stocks.setdefault(order_line.sku, queue.PriorityQueue()).empty():
+            return False
+        _, _, earliest_batch = self.available_stocks[order_line.sku].get()
+        success = earliest_batch.allocate(order_line)
+        if success:
+            if earliest_batch.available_qty == 0:
+                self.exhausted_stocks.setdefault(earliest_batch.sku, set()).add(earliest_batch)
+            else:
+                self.notify(earliest_batch)
+        return success
--- test_model.py
+++ test_model.py
@@ -1,32 +1,119 @@
+import time
 from datetime import date, timedelta
 import pytest
 
-# from model import ...
+from model import Batch, OrderLine, AllocationService, OrderReference
 
 today = date.today()
 tomorrow = today + timedelta(days=1)
 later = tomorrow + timedelta(days=10)
 
+def make_batch_and_line(sku: str, batch_qty: int, line_qty: int) -> tuple[Batch, OrderLine]:
+    return Batch(f'batch_{time.time()}', sku, batch_qty), OrderLine(OrderReference('order001'), sku, line_qty)
+
 
 def test_allocating_to_a_batch_reduces_the_available_quantity():
-    pytest.fail("todo")
+    batch, line = make_batch_and_line('small-red-table', 20, 5)
+
+    assert batch.can_allocate(line) is True
 
 
 def test_can_allocate_if_available_greater_than_required():
-    pytest.fail("todo")
+    batch, line = make_batch_and_line('small-red-table', 10, 5)
+
+    assert batch.can_allocate(line) is True
 
 
 def test_cannot_allocate_if_available_smaller_than_required():
-    pytest.fail("todo")
+    batch, line = make_batch_and_line('small-red-table', 5, 9)
+    
+    assert batch.can_allocate(line) is False
 
 
 def test_can_allocate_if_available_equal_to_required():
-    pytest.fail("todo")
+    batch, line = make_batch_and_line('small-red-table', 5, 5)
+
+    assert batch.can_allocate(line) is True
 
 
 def test_prefers_warehouse_batches_to_shipments():
-    pytest.fail("todo")
+    allocation = AllocationService()
+    batch_wh = Batch('batch_warehouse', 'small-red', 5, None)
+    batch_tomo = Batch('batch001', 'small-red', 10, tomorrow)
+    batch_later = Batch('batch002', 'small-red', 5, later)
+
+    allocation.notify(batch_later)
+    allocation.notify(batch_tomo)
+    allocation.notify(batch_wh)
+
+    order_line = OrderLine(OrderReference('order002'), 'small-red', 3)
+
+    allocation.put_order(order_line)
+
+    assert batch_wh.available_qty == 2
+    assert batch_tomo.available_qty == batch_tomo.qty
+    assert batch_later.available_qty == batch_later.qty
 
 
 def test_prefers_earlier_batches():
-    pytest.fail("todo")
+    allocation = AllocationService()
+    batch_tomo = Batch('batch001', 'small-red', 10, tomorrow)
+    batch_later = Batch('batch002', 'small-red', 5, later)
+
+    allocation.notify(batch_tomo)
+    allocation.notify(batch_later)
+
+    order_line = OrderLine(OrderReference('order002'), 'small-red', 2)
+
+    allocation.put_order(order_line)
+
+    assert batch_tomo.available_qty == 8
+    assert batch_later.available_qty == batch_later.qty
+
+def test_allocates_same_line_only_once():
+    batch, line = make_batch_and_line('small-red', 10, 2)
+
+    batch.allocate(line)
+
+    assert batch.can_allocate(line) is False
+
+def test_batch_rejects_line_with_different_sku():
+    batch = Batch('batch001', 'small-red', 10)
+    order_line = OrderLine(OrderReference('order002'), 'small-blue', 2)
+
+    assert batch.can_allocate(order_line) is False
+
+def test_allocation_rejects_already_allocated_line():
+    allocation = AllocationService()
+    batch = Batch('batch002', 'small-red', 10)
+
+    allocation.notify(batch)
+
+    order_line = OrderLine(OrderReference('order002'), 'small-red', 2)
+
+    allocation.put_order(order_line)
+
+    assert allocation.put_order(order_line) == False
+
+def test_allocation_fails_when_batches_are_exhausted():
+    batch = Batch('batch001', 'small-red', 10)
+    allocation = AllocationService()
+
+    allocation.notify(batch)
+
+    order_line1 = OrderLine(OrderReference('order002'), 'small-red', 10)
+
+    assert allocation.put_order(order_line1) == True
+
+    order_line2 = OrderLine(OrderReference('order002'), 'small-red', 2)
+
+    assert allocation.put_order(order_line2) == False
+
+def test_can_deallocate_only_allocated_lines():
+    batch, line = make_batch_and_line('small-red', 10, 2)
+
+    assert batch.can_deallocate(line) is False
+
+    batch.allocate(line)
+
+    assert batch.can_deallocate(line) is True
